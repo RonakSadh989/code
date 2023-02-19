97a06df70ae4b956176c47c6326ebc55ab0b7136
RonakSadh989 <124796081+RonakSadh989@users.noreply.github.com>
2023-02-19 17:57:42 +0530

    Update script.js

--- Blueprint/news api/news-home/script.js
+++ Blueprint/news api/news-home/script.js
@@ -12,12 +12,24 @@ async function getnews() {
   }
 }
 
-async function news(){
-    const data = await getnews();
-    let div = document.getElementById("headnews");
-    div.innerHTML =  `<img src="${await data[0].urlToImage}" alt="" height="80px">
-    <p >${await data[0].title +"<br>" +" ~By " + await data[0].author}
+async function news() {
+  const data = await getnews();
+  let div = document.getElementById("headnews");
+  div.innerHTML = `<img src="${await data[0].urlToImage}" alt="" height="80px">
+    <p >${(await data[0].title) + "<br>" + " ~By " + (await data[0].author)}
     <button class="btn1" ><a href="${await data[0].url}">See more..</a></button>
-    </p>`
+    </p>`;
 }
-news()
+news();
+
+function categ() {
+  let category = localStorage.getItem(localStorage.length - 1);
+
+  localStorage.setItem(
+    "category",
+    `https://newsapi.org/v2/everything?q=${category}&from=2023-01-19&sortBy=publishedAt&apiKey=247218c92d0b40c4b0573fe04571ab7c`
+  );
+
+  console.log(localStorage.getItem("category"));
+  window.location.href = "../news/index.html";
+}

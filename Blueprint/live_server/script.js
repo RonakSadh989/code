599bfeae420afa09ec3e2991143fa4b2356cd619
RonakSadh989 <124796081+RonakSadh989@users.noreply.github.com>
2023-02-21 12:44:34 +0530

    hhh

--- Blueprint/live_server/script.js
+++ Blueprint/live_server/script.js
@@ -1,18 +1,19 @@
 const http = require('http');
 const fs = require('fs');
-
+const { url } = require('inspector');
+  // const data = url('c:/html/Blueprint/news api/news-home/index.html')
 const server = http.createServer((req, res) => {
-  fs.readFile('/html/Blueprint/news api/news-home/index.html', (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.end(`Error reading file: ${err}`);
-      return;
-    }
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(data);
+  
+const requestListener = function (req, res) {
+    fs.readFile(__dirname + "../Button")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+};
+
   });
-});
 
 server.listen(80, '127.0.0.1', () => {
   console.log('Server running at http://127.0.0.1:80');
